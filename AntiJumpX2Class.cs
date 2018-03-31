16e94c209468d858a7d534715aa644345130413e
salva <salvadj1@hotmail.com>
2018-03-31 13:06:56 +0200

    Publish

---
+++ AntiJumpX2Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RustBuster2016;
+using UnityEngine;
+
+namespace AntiJumpX2
+{
+    public class AntiJumpX2Class : RustBuster2016.API.RustBusterPlugin
+    {
+        public override string Name { get { return "AntiJumpX2"; } }
+        public override string Author { get { return " by salva/juli"; } }
+        public override Version Version { get { return new Version("1.0"); } }
+
+        public Mono MonoClass;
+        public GameObject monoClassLoad;
+        public static AntiJumpX2Class Instance;
+
+        public override void Initialize()
+        {
+            Instance = this;
+            if (this.IsConnectedToAServer)
+            {
+                monoClassLoad = new GameObject();
+                MonoClass = monoClassLoad.AddComponent<Mono>();
+                UnityEngine.Object.DontDestroyOnLoad(monoClassLoad);
+                return;
+            }
+        }
+        public override void DeInitialize()
+        {
+            if (monoClassLoad != null) UnityEngine.Object.DestroyImmediate(monoClassLoad);
+        }
+    }
+}
---
+++ Mono.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AntiJumpX2
+{
+    public class Mono : MonoBehaviour
+    {
+        public DateTime LastAltPush;
+        public DateTime LastSpacePush;
+        public Vector3 LastPlayerLocation;
+
+        public void Update()
+        {
+            if (Controllable.localPlayerControllableExists)
+            {
+                var pl = PlayerClient.GetLocalPlayer().controllable.GetComponent<Character>();
+
+                if (Input.GetKeyDown(KeyCode.LeftAlt))
+                {
+                    LastPlayerLocation = pl.transform.position;
+                    LastAltPush = System.DateTime.Now;
+
+                    //Rust.Notice.Inventory("","LeftALT" + LastAltPush.ToString());//
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    //Rust.Notice.Inventory("", "Space" + LastSpacePush.ToString());
+                    LastSpacePush = System.DateTime.Now;
+                    TimeSpan ts = LastSpacePush - LastAltPush;
+                    if (ts.Seconds < 2)
+                    {
+                        //quizas aqui deba de comprobar que el jugador no este mas lejos de 100m entr las 2 posiciones registradas para evitar confundirse con un TP
+
+                        // /////////////////////
+
+                        pl.ccmotor.Teleport(new Vector3(LastPlayerLocation.x, LastPlayerLocation.y, LastPlayerLocation.z));
+                        //Rust.Notice.Popup("", "-2 segundos detectados " + ts.Seconds.ToString());
+                        Rust.Notice.Popup("", "Super Jump are Blocked");
+                    }
+                }
+            }
+        }
+    }
+}
